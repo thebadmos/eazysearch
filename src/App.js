91ecaca0b6d4ab1672b4ce38680533083d196f4f
thebadmos <badmosolamiji@gmail.com>
2021-11-12 13:51:19 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Allroutes from './routes/Allroutes';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Router>
-
+     
      <Allroutes/>
-     </Router>
+  
     </div>
   );
 }
